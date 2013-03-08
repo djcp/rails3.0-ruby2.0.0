c1ef484d984073c976fa05f3b64d613ec1e01765
Dan Collis-Puro <djcp@thoughtbot.com>
2013-03-08 15:59:54 -0500

    User controller functional tests busted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '6d5f7776467e056e730fec0be9b71936a1428ba908e38d4afd363918d6185fa2ecda3ec26aca6b82eee9766fb1de98cefbcb03cb803cdeafcab03a5064819f7e'
